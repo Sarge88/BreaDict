d3ab9ce6342c29ba989865c96669e067952b9a03
ab98el <ab98el@gmail.com>
2020-04-05 15:57:45 +0200

    [Feature] get input with a dialog

--- app/src/main/java/com/example/breadict/MainActivity.java
+++ app/src/main/java/com/example/breadict/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.breadict;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    //test
+    Button addBtn;
+    EditText input;
 
     TextView myTextView;
     int count=0;
@@ -65,10 +71,42 @@ public class MainActivity extends AppCompatActivity {
 
         TableRow.LayoutParams  params1=new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT,1.0f);
         TableLayout table = findViewById(R.id.table);
-        Button add = findViewById(R.id.addNewWord);
 
-        TextView txt1=new TextView(this);
-        txt1.setText("asd");
+        final TextView txt1=new TextView(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New word");
+        builder.setIcon(R.drawable.ic_launcher_background);
+        builder.setMessage("Please add a word.");
+
+        input = new EditText(this);
+        builder.setView(input);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String txt = input.getText().toString();
+                txt1.setText(txt);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        final AlertDialog ad = builder.create();
+
+        addBtn = (Button) findViewById(R.id.addNewWord);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ad.show();
+            }
+        });
+
 
         TableRow newRow = new TableRow(this);// add views to the row
         newRow.addView(txt1);
