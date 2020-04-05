bc724979f3e4a5b5a46e58cdcaad06471040ef85
Kalapis Gábor <kgabor0420@gmail.com>
2020-04-05 18:04:51 +0200

    Fix "add 2 word" feature.

--- app/src/main/java/com/example/breadict/MainActivity.java
+++ app/src/main/java/com/example/breadict/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     Button addBtn;
     EditText input;
 
+
     TextView myTextView;
     int count=0;
 
@@ -34,30 +38,35 @@ public class MainActivity extends AppCompatActivity {
 
         myTextView=(TextView)findViewById(R.id.timer);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("New word");
-        builder.setIcon(R.drawable.ic_launcher_background);
-        builder.setMessage("Please add a word.");
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("New word");
+        dialog.setIcon(R.drawable.ic_launcher_background);
+        dialog.setMessage("Please add a word.");
 
         input = new EditText(this);
-        builder.setView(input);
+        dialog.setView(input);
 
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+        //input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
+
+
+
+        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                String txt = input.getText().toString();
-                addNewWord(txt);
+                String szo = input.getText().toString();
+                String[] tomb = szo.split(" ");
+                addNewWord(tomb[0],tomb[1]);
             }
         });
 
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
             }
         });
 
-        final AlertDialog ad = builder.create();
+        final AlertDialog ad = dialog.create();
 
         addBtn = (Button) findViewById(R.id.addNewWord);
         addBtn.setOnClickListener(new View.OnClickListener() {
@@ -76,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run()
                     {
-                        myTextView.setText("How many seconds did I waste with running this app: "+count);
+                        myTextView.setText("How many seconds did I study: "+count);
                         count++;
                     }
                 });
@@ -100,21 +109,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void addNewWord(String txt){
+    public void addNewWord(String input1, String input2){
 
         TextView txt1 = new TextView(this);
+        TextView txt2 = new TextView(this);
 
-        txt1.setText(txt);
+        txt1.setText(input1);
+        txt2.setText(input2);
 
-        TableRow.LayoutParams  params1=new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT,1.0f);
+        //TableRow.LayoutParams  params1=new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT,1.0f);
         TableLayout table = findViewById(R.id.table);
 
         TableRow newRow = new TableRow(this);// add views to the row
         newRow.addView(txt1);
+        newRow.addView(txt2);
 
         newRow.addView(new TextView(this)); // you would actually want to set properties on this before adding it
 
         // add the row to the table layout
+        //table.addView(params1, new TableLayout.LayoutParams());
         table.addView(newRow);
     }
 
