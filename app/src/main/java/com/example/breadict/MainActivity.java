2a79350c5ca6cfe053d5e97c3a058a961afc0256
Levente Váczi <vaczlevente99@gmail.com>
2020-04-18 18:16:38 +0200

    Colorize function

--- app/src/main/java/com/example/breadict/MainActivity.java
+++ app/src/main/java/com/example/breadict/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -23,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button addBtn;
     EditText input;
-
-
+    Button colorizeBtn;
+    EditText input2;
     TextView myTextView;
     int count=0;
 
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         myTextView=(TextView)findViewById(R.id.timer);
 
         addDialog();
+        colorizeDialog();
 
         Timer T=new Timer();
         T.scheduleAtFixedRate(new TimerTask() {
@@ -128,6 +131,78 @@ public class MainActivity extends AppCompatActivity {
 
         addBtn = (Button) findViewById(R.id.addNewWord);
         addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ad.show();
+            }
+        });
+    }
+
+    public void colorize(String word, String color){
+        TableLayout table = findViewById(R.id.table);
+        int n = 0;
+        for (int i = 1; i < table.getChildCount(); i++) {
+            View child = table.getChildAt(i);
+
+            if (child instanceof TableRow) {
+                TableRow row = (TableRow) child;
+
+                for (int x = 1; x < row.getChildCount(); x=x+2) {
+                    View view = row.getChildAt(x);
+                    View view2 = row.getChildAt(x-1);
+                    TextView text = (TextView) view;
+                    String s = (String) text.getText().toString();
+                    boolean correct = word.equals(s);
+                    if (correct){
+                        if (color.equals("red")){
+                            view.setBackgroundColor(0xFFFA8072);
+                            view2.setBackgroundColor(0xFFFA8072);
+                        }
+                        else if (color.equals("green")){
+                            view.setBackgroundColor(0xFF00FF00);
+                            view2.setBackgroundColor(0xFF00FF00);
+                        }
+                        else if(color.equals("yellow")){
+                            view.setBackgroundColor(0xFFFFFF00);
+                            view2.setBackgroundColor(0xFFFFFF00);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    public void colorizeDialog(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Colorize");
+        dialog.setIcon(R.drawable.ic_launcher_background);
+        dialog.setMessage("[Word] - [Color]");
+
+        input2 = new EditText(this);
+        dialog.setView(input2);
+
+        //input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
+
+
+
+        dialog.setPositiveButton("Colorize", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String szo = input2.getText().toString();
+                String[] tomb = szo.split("-");
+                colorize(tomb[0],tomb[1]);
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        final AlertDialog ad = dialog.create();
+
+        colorizeBtn = (Button) findViewById(R.id.colorize);
+        colorizeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ad.show();
